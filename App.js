3671817afc22463198f0a93251784a6c05d560d5
jeffcfhung <jeff.cf.hung@gmail.com>
2018-08-04 16:23:54 -0700

    Fix js lint errors

--- App.js
+++ App.js
@@ -1,83 +1,87 @@
 import React from 'react';
+
 import {
-  Dimensions,
-  WebView,
-  View,
-  Text,
-} from 'react-native';
+    View,
+    Text,
+    PanResponder,
+    Animated,
+    Dimensions,
+    Image,
+    WebView
+} from 'react-native'
+
+GLOBAL = require('./app/lib/globals');
 import SteerController from './app/components/steer';
 import CameraController from './app/components/camera';
 import FaceRecController from './app/components/face';
 
-const GLOBAL = require('./app/lib/globals');
-
-const styles = require('./app/config/styles');
+let styles = require('./app/config/styles');
 
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      debugLog: 'Messages',
-      isPortrait: true,
-    };
-    this.logs = [];
+      super(props);
+
+      this.state = {
+          debugLog: 'Messages',
+          isPortrait: true
+      }
+      this.logs = [];
+  }
+
+  onLayout(e) {
+      const {width, height} = Dimensions.get('window');
+      let isPortrait = height > width;
+      if (isPortrait) {
+          styles.video = [{
+              width: 400,
+              height: 300
+          }]
+      }
+      else {
+          styles.video = [{
+              width: 480,
+              height: 360
+          }]
+      }
+
+      this.setState({isPortrait: isPortrait});
   }
 
   // FIXME: Apply observers instead of direct accesss. This is just quick hacking
   componentDidMount() {
-    GLOBAL.CUSTOM_EVENT.ViewPort = this;
-    GLOBAL.CUSTOM_EVENT.setDebugLog = this.setDebugLog;
+      GLOBAL.CUSTOM_EVENT.ViewPort = this;
+      GLOBAL.CUSTOM_EVENT.setDebugLog = this.setDebugLog;
   }
 
-  onLayout() {
-    const { width, height } = Dimensions.get('window');
-    const isPortrait = height > width;
-    if (isPortrait) {
-      styles.video = [{
-        width: 400,
-        height: 300,
-      }];
-    } else {
-      styles.video = [{
-        width: 480,
-        height: 360,
-      }];
-    }
-
-    this.setState({ isPortrait });
+  setDebugLog(log) {
+      log = new Date().toISOString().replace(/.*T/, ' ').replace(/Z.*/, ' ') + log;
+      let logs = GLOBAL.CUSTOM_EVENT.ViewPort.logs;
+      logs.unshift(log);
+      if (logs.length > 4) {
+          logs.splice(-1,1);
+      }
+      GLOBAL.CUSTOM_EVENT.ViewPort.setState({debugLog: logs.join('\n')});
   }
 
-  setDebugLog(input) {
-    const log = new Date().toISOString().replace(/.*T/, ' ').replace(/Z.*/, ' ') + input;
-    const logs = GLOBAL.CUSTOM_EVENT.ViewPort.logs;
-    logs.unshift(log);
-    if (logs.length > 4) {
-      logs.splice(-1, 1);
-    }
-    GLOBAL.CUSTOM_EVENT.ViewPort.setState({ debugLog: logs.join('\n') });
-  }
-
-  static getVideoHtml() {
-    return (
-      `<html><body><img src="${GLOBAL.VIDEO_URL}" width="100%" style="background-color: white; min-height: 100%; min-width: 100%; position: fixed; top: 0; left: 0;"></body></html>`
-    );
+  getVideoHtml() {
+      return ('<html><body><img src="' + GLOBAL.VIDEO_URL + '" width="100%" style="background-color: white; min-height: 100%; min-width: 100%; position: fixed; top: 0; left: 0;"></body></html>');
   }
 
   getDebugComponent() {
-    if (this.state.isPortrait) {
-        return (
-            <View style={styles.tabBar}>
-                <Text>{this.state.debugLog}</Text>
-            </View>
-        );
-    }
-}
+      if (this.state.isPortrait) {
+          return (
+              <View style={styles.tabBar}>
+                  <Text>{this.state.debugLog}</Text>
+              </View>
+          );
+      }
+  }
 
-render() {
-    return (
-        <View onLayout={this.onLayout.bind(this)} style={styles.mainContainer}>
-            <View style={this.state.isPortrait && styles.navBar}>
+  render() {
+      return (
+          <View onLayout={this.onLayout.bind(this)} style={styles.mainContainer}>
+              <View style={this.state.isPortrait && styles.navBar}>
                   <Text style={styles.navBarButton}>Back</Text>
                   <Text style={styles.navBarHeader}>Smart Car</Text>
                   <Text style={styles.navBarButton}>More</Text>
@@ -90,7 +94,7 @@ render() {
                          scalesPageToFit={true}
                          startInLoadingState={false}
                          scrollEnabled={true}
-                         source={{html: App.getVideoHtml(), baseUrl: '/'}} />
+                         source={{html: this.getVideoHtml(), baseUrl: '/'}} />
                       <FaceRecController name='Unknown' data={this.state} />
                   </View>
                   <CameraController name='Camera Control' data={this.state} />
--- app/lib/globals.js
+++ app/lib/globals.js
@@ -1,19 +1,11 @@
 // TODO: Move to observor patteren. This is a quick hack.
-const MOCK = false;
-let BASE_URL;
-let VIDEO_URL;
-
-if (MOCK) {
-  BASE_URL = 'http://localhost';
-  VIDEO_URL = `${BASE_URL}:8000/static/img.jpg`;
-} else {
-  BASE_URL = 'http://10.42.0.1';
-  VIDEO_URL = `${BASE_URL}:8000/static/img.jpg`;
-}
+let BASE_URL = 'http://10.42.0.1';
+let VIDEO_URL = BASE_URL + ':8080/?action=stream';
 module.exports = {
-  BASE_URL,
-  VIDEO_URL,
-  // ML_URL: 'http://192.168.56.101',
-  ML_URL: 'http://10.42.0.220',
-  CUSTOM_EVENT: {},
+    BASE_URL: BASE_URL,
+    VIDEO_URL: VIDEO_URL,
+    //ML_URL: 'http://192.168.56.101',
+    ML_URL: 'http://10.42.0.132',
+    CUSTOM_EVENT: {},
 };
+
